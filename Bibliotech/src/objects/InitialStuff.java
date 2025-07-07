2350386344d036787b60f86221b1b9c67f508143
Maershmaellow <brooksmarcelo03@gmail.com>
2025-07-06 23:08:47 -0700

    last minute file testing (Initial Stuff)

--- Bibliotech/src/objects/InitialStuff.java
+++ Bibliotech/src/objects/InitialStuff.java
@@ -1,17 +1,26 @@
 package objects;
 
+import java.time.LocalDate;
+
 public class InitialStuff {
 	private static void initialUsers() {
-		
+		DataBase.users.add(new User("Bob", "password123", false));
+        DataBase.users.add(new User("Phil", "mypass456", false));
+        DataBase.users.add(new User("Johnathan", "student789", false));
 	}
 	
 	private static void initialBooks() {
-		books.add(new Book("Campbell Biology", "Jane Reece", "Natural Science", "Excellent", 275.00, 1002));
-        books.add(new Book("Chemistry: The Central Science", "Theodore Brown", "Natural Science", "Good", 198.99, 1004));
+		// String title, String author, String genre, String condition, double price, boolean sold, String seller
+		DataBase.books.add(new Book("Database Stuff", "Tommy Hilfiger", "Computer Science", "Good", 48.50, false,"Bob"));
+        DataBase.books.add(new Book("Web Dev Fundamentals", "Mark Johnsohn", "Computer Science", "Fair", 42.00, false, "Phil"));
+		
+		DataBase.books.add(new Book("Campbell Biology", "Jane Reece", "Natural Science", "Excellent", 275.00, false, "Johnathan"));
+        DataBase.books.add(new Book("Chemistry: The Central Science", "Theodore Brown", "Natural Science", "Good", 198.99, false, "Bob"));
 	}
 	
 	private static void initialTransactions() {
-		transactions.add(new Transaction("TXN003", 1004, 1002, "Statistics for Engineers", 78.25, LocalDate.of(2024, 12, 8), "Completed"));
-        transactions.add(new Transaction("TXN004", 1002, 1003, "Modern Physics", 165.75, LocalDate.of(2024, 12, 5), "Pending"));
+		//String transactionId, String buyerId, String sellerId, String bookTitle, double cost, String status, LocalDate Date
+		DataBase.transaction.add(new Transaction("TXN003", "Bob", "Phil", "Statistics and Analysis", 78.25, "Completed", LocalDate.of(2024, 12, 8)));
+        DataBase.transaction.add(new Transaction("TXN004", "Johnathan", "Phil", "Modern Physics", 165.75, "Pending", LocalDate.of(2024, 12, 5)));
 	}
 }
